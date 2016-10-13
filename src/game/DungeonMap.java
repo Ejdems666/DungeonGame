75db38d23ba804c38a96c5f461ef3662a582cf9e
Ejdems <becvar.adam666@gmail.com>
2016-10-13 09:10:12 +0200

    refractoring

--- src/game/DungeonMap.java
+++ src/game/DungeonMap.java
@@ -39,22 +39,22 @@ public class DungeonMap {
     private Room getRoom(String roomMapper) {
         if (roomMapper.charAt(0) == '0') {
             return null;
-        } else {
-            String[] extrasMapper = roomMapper.substring(2).split("-");
-            float gold = getGold(extrasMapper);
-            Room room = new Room(gold);
-            try {
-                setDirections(extrasMapper[0], room);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.exit(1);
-            }
-            return room;
         }
+        String[] extrasMapper = roomMapper.substring(2).split("-");
+        float gold = getGold(extrasMapper);
+        Room room = new Room(gold);
+        try {
+            setDirections(extrasMapper[0], room);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+        return room;
+
     }
 
     private float getGold(String[] extrasMapper) {
-        if(extrasMapper.length >= 2) {
+        if (extrasMapper.length >= 2) {
             return Float.parseFloat(extrasMapper[1]);
         }
         return random.nextInt(10);
@@ -65,13 +65,13 @@ public class DungeonMap {
             char direction = directionMapper.charAt(i);
             boolean correct = false;
             for (Room.Direction d : Room.Direction.values()) {
-                if(d.getValue() == direction) {
+                if (d.getValue() == direction) {
                     room.setDirection(d);
                     correct = true;
                 }
             }
-            if(!correct) {
-                throw new Exception("Wrong direction: "+direction);
+            if (!correct) {
+                throw new Exception("Wrong direction: " + direction);
             }
         }
     }
